2ba722fa8b44e31628cf320d80f02e99e371b589
Zi heng Liu <84878485+ZihengL@users.noreply.github.com>
2026-01-03 17:53:21 -0500

    Cleaned up FXML file
    Also implemented the Quit functionality from the MenuBar

--- src/main/java/io/github/zihengl/demineur/controllers/command/QuitCommand.java
+++ src/main/java/io/github/zihengl/demineur/controllers/command/QuitCommand.java
@@ -1,11 +1,12 @@
 package io.github.zihengl.demineur.controllers.command;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 
 public class QuitCommand implements Command<ActionEvent>  {
 
     @Override
     public void handle(ActionEvent event) {
-
+        Platform.exit();
     }
 }
